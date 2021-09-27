306a5aefea3e35bff921ae3e262f9674ac379730
Pavel <www.crank-06@mail.ru>
2021-09-28 00:04:57 +0300

    поставил postgreSQL

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -75,16 +75,20 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test1',
+        'USER': 'paul',
+        'PASSWORD': 'q1e2t37',
+    }
+}
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
